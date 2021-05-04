00653d378575b71c89522ee6e8de06abf4cf5fed
Pooja <pooja.14206@gmail.com>
2021-05-04 20:46:37 +0530

    Added a file that Demonestrate file operations

--- src/test/java/com/bridgelabz/NIOFileAPITest.java
+++ src/test/java/com/bridgelabz/NIOFileAPITest.java
@@ -10,5 +10,37 @@ import java.nio.file.Paths;
 import java.util.stream.IntStream;
 
 public class NIOFileAPITest {
+    private static String HOME = System.getProperty("user.home");
+    private static String PLAY_WITH_NIO = "TempPlayGround";
 
+    @Test
+    public void givenPath_WhenChecked_ThenConfirm() throws IOException {
+        //check file Exists
+        Path homePath = Paths.get(HOME);
+        Assert.assertTrue(Files.exists(homePath));
+
+        //delete file and check file not exist
+        Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        if(Files.exists(playPath)) FileUtils.deleteFiles(playPath.toFile());
+        Assert.assertTrue(Files.notExists(playPath));
+
+        //create Directory
+        Files.createDirectory(playPath);
+        Assert.assertTrue(Files.exists(playPath));
+
+        //create File
+        IntStream.range(1,10).forEach(cntr -> {
+            Path tempFile = Paths.get(playPath + "/temp" + cntr);
+            Assert.assertTrue(Files.notExists(tempFile));
+            try {Files.createFile(tempFile);}
+            catch (IOException e) { }
+            Assert.assertTrue(Files.exists(tempFile));
+        });
+
+        //List files, Directories as well as Files With Extension
+        Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+        Files.newDirectoryStream(playPath).forEach(System.out::println);
+        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() &&
+                path.toString().startsWith("temp")).forEach(System.out::println);
+    }
 }
