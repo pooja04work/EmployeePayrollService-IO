2f0c5ffd313ddfe79dd8e8fbd98a7d19b04d1fbf
Pooja <pooja.14206@gmail.com>
2021-05-05 00:20:51 +0530

    added a file that store employee payroll in a file

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-    public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+
+    public enum IOService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+    }
+
     private List<EmployeePayrollData> employeePayrollList;
 
     public EmployeePayrollService() {}
@@ -19,7 +23,7 @@ public class EmployeePayrollService {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.readEmployeePayrollData(consoleInputReader);
-        employeePayrollService.writeEmployeePayrollData();
+        employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
     }
 
     private void readEmployeePayrollData(Scanner consoleInputReader){
@@ -32,7 +36,21 @@ public class EmployeePayrollService {
         employeePayrollList.add(new EmployeePayrollData(id, name, salary));
     }
 
-    private void writeEmployeePayrollData() {
-        System.out.println("\nWriting enployee payroll Roaster to Console\n" + employeePayrollList);
+    public void writeEmployeePayrollData(IOService ioService) {
+        if (ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("\nWriting enployee payroll Roaster to Console\n" + employeePayrollList);
+        else if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().writeData(employeePayrollList);
+    }
+
+    public void printData(IOService ioService){
+        if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().printData();
+    }
+
+    public long countEntries(IOService ioService) {
+        if (ioService.equals(IOService.FILE_IO))
+            return new EmployeePayrollFileIOService().countEntries();
+        return 0 ;
     }
 }
---
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Arrays;
+
+import static com.bridgelabz.EmployeePayrollService.IOService.FILE_IO;
+
+//import static com.bridgelabz.EmployeePayrollService.IOService.FILE_IO;
+public class EmployeePayrollServiceTest {
+   @Test
+    public void given3EmployeeWhenWrittenToFile_shouldMatchEmployeeEntries() {
+            EmployeePayrollData[] arrayOfEmps = {
+                    new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                    new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                    new EmployeePayrollData(3, "Sundar Pichai", 300000.0)
+            };
+            EmployeePayrollService employeePayrollService;
+            employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+            employeePayrollService.writeEmployeePayrollData(FILE_IO);
+            employeePayrollService.printData(FILE_IO);
+            long entries = employeePayrollService.countEntries(FILE_IO);
+            Assert.assertEquals(3, entries);
+    }
+}
